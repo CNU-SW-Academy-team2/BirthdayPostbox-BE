43778a7e0eae1f0756cceb9b0d60e404e6d0cd76
Yujin-nKim <yujinkimvv@gmail.com>
2023-02-20 22:19:16 +0900

    initial project setting

---
+++ src/main/java/com/project/BirthdayPostbox/BirthdayPostboxApplication.java
@@ -0,0 +1,13 @@
+package com.project.BirthdayPostbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirthdayPostboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirthdayPostboxApplication.class, args);
+	}
+
+}
