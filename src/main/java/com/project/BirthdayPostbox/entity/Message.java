5cbb018c096f6e57aceb84e0f8a25962d2b1229e
minsu <rnjsals1541@naver.com>
2023-02-27 11:20:57 +0900

    Message entity

--- src/main/java/com/project/BirthdayPostbox/entity/Message.java
+++ src/main/java/com/project/BirthdayPostbox/entity/Message.java
@@ -1,4 +1,34 @@
 package com.project.BirthdayPostbox.entity;
 
 public class Message {
+    private String messageId;
+
+    private String roomId;
+
+    private String messageSender;
+
+    private String messageContent;
+
+    public Message(String messageId, String roomId, String messageSender, String messageContent) {
+        this.messageId = messageId;
+        this.roomId = roomId;
+        this.messageSender = messageSender;
+        this.messageContent = messageContent;
+    }
+
+    public String getMessageId() { return messageId; }
+
+    public void setMessageId(String messageId) { this.messageId = messageId; }
+
+    public String getRoomId() { return roomId; }
+    
+    public void setRommId(String roomId) { this.roomId = roomId; }
+
+    public Stirng getMessageSender() { return messageSender; }
+
+    public void setMessageSender(String messageSender) { this.messageSender = messageSender; }
+
+    public String getMessageContent() { return messageContent; }
+
+    public void setMessageSender(String messageSender) { this.messageSender = messageSender; }
 }
