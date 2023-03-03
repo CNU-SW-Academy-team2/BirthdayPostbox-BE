e5d14ab2c1134dda7028d0fdf0cf1b9979d730e2
ShinYooJung <shinyoojung0@gmail.com>
2023-03-03 19:55:46 +0900

    Lombok dependency 추가 Message, Present Entity 수정

--- src/main/java/com/project/BirthdayPostbox/entity/Message.java
+++ src/main/java/com/project/BirthdayPostbox/entity/Message.java
@@ -1,4 +1,23 @@
 package com.project.BirthdayPostbox.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "message")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Message {
+    @Id
+    private String messageId;
+    private String roomId;
+    private String messageSender;
+    private String messageContent;
 }
--- src/main/java/com/project/BirthdayPostbox/entity/Present.java
+++ src/main/java/com/project/BirthdayPostbox/entity/Present.java
@@ -1,58 +1,25 @@
 package com.project.BirthdayPostbox.entity;
 
-public class Present {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@Table(name = "present")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Present {
+    @Id
     private String presentId;
     private String roomId;
     private String presentSender;
     private String presentContent;
     private String presentImgUrl;
 
-    public Present(String presentId, String roomId, String presentSender, String presentContent, String presentImgUrl) {
-        this.presentId = presentId;
-        this.roomId = roomId;
-        this.presentSender = presentSender;
-        this.presentContent = presentContent;
-        this.presentImgUrl = presentImgUrl;
-    }
-
-    public String getPresentId() {
-        return presentId;
-    }
-
-    public void setPresentId(String presentId) {
-        this.presentId = presentId;
-    }
-
-    public String getRoomId() {
-        return roomId;
-    }
-
-    public void setRoomId(String roomId) {
-        this.roomId = roomId;
-    }
-
-    public String getPresentSender() {
-        return presentSender;
-    }
-
-    public void setPresentSender(String presentSender) {
-        this.presentSender = presentSender;
-    }
-
-    public String getPresentContent() {
-        return presentContent;
-    }
-
-    public void setPresentContent(String presentContent) {
-        this.presentContent = presentContent;
-    }
-
-    public String getPresentImgUrl() {
-        return presentImgUrl;
-    }
-
-    public void setPresentImgUrl(String presentImgUrl) {
-        this.presentImgUrl = presentImgUrl;
-    }
 }
