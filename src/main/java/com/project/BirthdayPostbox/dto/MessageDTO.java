2cbc71d5edf22f35d39b35ed77f277fdae347096
Yujin-nKim <yujinkimvv@gmail.com>
2023-03-15 20:50:31 +0900

    fix roomDTO

--- src/main/java/com/project/BirthdayPostbox/dto/MessageDTO.java
+++ src/main/java/com/project/BirthdayPostbox/dto/MessageDTO.java
@@ -14,14 +14,12 @@ public class MessageDTO {
     private String messageSender;
     private String messageContent;
     private RoomDTO roomDTO;
-    private String room_id;
 
     public static MessageDTO toMessageDTO(MessageEntity messageEntity) {
         MessageDTO messageDTO = new MessageDTO();
         messageDTO.setMessageId(messageEntity.getMessageId());
         messageDTO.setMessageSender(messageEntity.getMessageSender());
         messageDTO.setMessageContent(messageEntity.getMessageSender());
-        messageDTO.setRoom_id(messageEntity.getMessageId());
 
         return messageDTO;
     }
